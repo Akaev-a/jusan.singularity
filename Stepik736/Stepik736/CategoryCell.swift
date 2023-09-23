cf5ef089492e8931ea729b5cb6540ce9eefd8980
Akaev <akaev@MacBook-Pro-Akaev.local>
2023-09-24 00:06:14 +0600

    Сделал первый экран

---
+++ Stepik736/Stepik736/CategoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCell.swift
+//  Stepik736
+//
+//  Created by Akaev on 23/9/2023.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
---
+++ Stepik736/Stepik736/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stepik736
+//
+//  Created by Akaev on 23/9/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
